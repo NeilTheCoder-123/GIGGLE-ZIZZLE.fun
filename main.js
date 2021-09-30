98e69e9f53fbe92a06bfbdc6a5b5d8ab24010f1c
NeilTheCoder-123 <78020134+NeilTheCoder-123@users.noreply.github.com>
2021-09-30 17:54:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,9 +11,23 @@ function setup(){
 function draw(){
     image(video, 0, 0, 640, 480);
 
+    stroke(50, 168, 82);
+    fill(50, 168, 82);
+
+    rect(150, 90, 375, 25);
+    rect(150,350, 375, 25);
+    rect(150, 90, 25, 275);
+    rect(500, 90, 25, 275);
+
+    stroke(168, 10, 10);
+    fill(168, 10, 10);
+    
+    circle(170,102,50);
+    circle(510,102,50);
+    circle(170,360,50);
+    circle(510,360,50);
 }
 
 function take_snapshot(){
     save('Neil.png');
 }
-
